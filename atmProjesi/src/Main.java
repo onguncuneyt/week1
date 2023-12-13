938df558c3d6d00ac62c424254f95045159ce415
cinoxil <c.ugurongun@gmail.com>
2023-12-14 02:06:30 +0300

    atmProjesi release

---
+++ atmProjesi/src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //scanner class init
+        Scanner input = new Scanner(System.in);
+
+        //variables defined
+        String userName, password;
+        int right = 3;
+        int balance = 1500;
+        int select;
+        int price=0;
+        while (right > 0) {
+            System.out.print("Kullanıcı Adı :");
+            userName = input.nextLine();
+            System.out.print("Parola : ");
+            password = input.nextLine();
+
+            //check username and pass correct
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                //inf loop untill press 4
+                do {
+                    System.out.println("1-Para yatırma\n" +
+                            "2-Para Çekme\n" +
+                            "3-Bakiye Sorgula\n" +
+                            "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = input.nextInt();
+
+                    //switch for select menu items
+                    switch (select){
+                        case 1:
+                            System.out.print("Para miktarı : ");
+                            price = input.nextInt();
+                            balance += price;
+                            break;
+                        case 2:
+                            System.out.print("Para miktarı : ");
+                            price = input.nextInt();
+                            if (price > balance) {
+                                System.out.println("Bakiye yetersiz.");
+                            } else {
+                                balance -= price;
+                            }
+                            break;
+                        case 3:
+                            System.out.println("Bakiyeniz : " + balance);
+                            break;
+                    }
+                } while (select != 4);
+                System.out.println("Tekrar görüşmek üzere.");
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+                }
+            }
+        }
+    }
+}
